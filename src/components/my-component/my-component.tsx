6818e82b48f3904b420db7d4906b2b241d4e4f95
Robert Ramsey <robert-ramsey@uiowa.edu>
2024-10-02 08:41:18 -0500

    initial commit to show code

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Element, Component, Prop, h, Host } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
@@ -7,6 +7,7 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
+  @Element() el: HTMLElement;
   /**
    * The first name
    */
@@ -15,18 +16,56 @@ export class MyComponent {
   /**
    * The middle name
    */
-  @Prop() middle: string;
+  @Prop({ mutable: true }) middle: string;
 
   /**
    * The last name
    */
   @Prop() last: string;
 
+  public updateField(field = ''): void {
+    const val: HTMLInputElement = this.el.shadowRoot.getElementById(field) as HTMLInputElement;
+    if (val === null) {
+      console.log('Element with does not exist: ', field);
+      return;
+    }
+    console.log('updating ', field, ' with ', val.value);
+    if (field === 'fi') {
+      this.first = val.value;
+    } else if (field === 'mi') {
+      this.middle = val.value;
+    } else if (field === 'la') {
+      this.last = val.value;
+    } else {
+      console.log('Bad field ',  field);
+    }
+  }
+
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <Host>
+        <div>
+          Hello, World! I'm {this.getText()}
+        </div>
+        <div>
+          <input id="fi" name="fi" type="text" />
+          <button onClick={ () => {this.updateField('fi')} }>
+            update Prop() value first
+          </button><br />
+          <input id="mi" name="mi" type="text" />
+          <button onClick={ () => {this.updateField('mi')} }>
+            update mutable Prop value middle
+          </button><br />
+          <input id="la" name="la" type="text" />
+          <button onClick={ () => {this.last = 'something else' } }>
+            update Prop() last to something else
+          </button>
+        </div>
+      </Host>
+    );
   }
 }
